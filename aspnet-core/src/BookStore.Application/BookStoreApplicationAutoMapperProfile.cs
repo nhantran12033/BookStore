76688e028ab8c57fc180f16ec2e9a29af52c5945
nhantran12033 <nhantran12033@gmail.com>
2023-05-29 21:23:14 +0700

    Update bookstore

---
+++ aspnet-core/src/BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Books;
+using AutoMapper;
+
+
+namespace BookStore;
+
+public class BookStoreApplicationAutoMapperProfile : Profile
+{
+    public BookStoreApplicationAutoMapperProfile()
+    {
+        CreateMap<Book, BookDto>();
+        CreateMap<CreateUpdateBookDto, Book>();
+    }
+}
